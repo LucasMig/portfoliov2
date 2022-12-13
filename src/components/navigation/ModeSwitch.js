61c8d62218349ad944d901756ae9d699dc15ad9d
LucasMig <lucasmigliori@gmail.com>
2022-12-12 23:42:08 -0300

    fixing side effects and imports

--- src/components/navigation/ModeSwitch.js
+++ src/components/navigation/ModeSwitch.js
@@ -1,27 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FaRegSun, FaRegMoon } from "react-icons/fa";
 
 const ModeSwitch = () => {
-  const [theme, setTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   const toggleMode = () => {
-    const newState = !theme;
-    setTheme(newState);
-  };
-
-  // useEffect(() => {
-  //   setTheme(false);
-  // }, []);
-
-  useEffect(() => {
+    const newState = !isDarkTheme;
+    setIsDarkTheme(newState);
     document.body.classList.toggle("dark");
-  }, [theme]);
+  };
 
   return (
     <div className="mode__switch" onClick={toggleMode}>
       <FaRegSun />
       <FaRegMoon />
-      <button className={`btn toggle ${theme ? "dark" : "light"}`}></button>
+      <button
+        className={`btn toggle ${isDarkTheme ? "dark" : "light"}`}
+      ></button>
     </div>
   );
 };
